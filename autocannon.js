f791d619d467e79bf7b1e60b217001e0f6caa4df
Matteo Collina <hello@matteocollina.com>
2016-04-02 10:22:27 +0200

    Added tracking of status codes.

--- autocannon.js
+++ autocannon.js
@@ -11,15 +11,48 @@ function run (opts, cb) {
   const latencies = new Histogram(1, 10000, 3)
   const requests = new Histogram(1, 1000000, 3)
   const throughput = new Histogram(1, 1000000000, 1)
+  const statusCodes = [
+    0, // 1xx
+    0, // 2xx
+    0, // 3xx
+    0, // 4xx
+    0  // 5xx
+  ]
+
   const url = URL.parse(opts.url)
+
+  opts.pipelining = opts.pipelining || 1
+
   let counter = 0
   let bytes = 0
   let errors = 0
+  let totalBytes = 0
+  let totalRequests = 0
+  let stop = false
+
   const interval = setInterval(() => {
+    totalBytes += bytes
+    totalRequests += counter
     requests.record(counter)
     throughput.record(bytes)
     counter = 0
     bytes = 0
+
+    if (stop) {
+      clearInterval(interval)
+      clients.forEach((client) => client.destroy())
+      cb(null, {
+        requests: histAsObj(requests, totalRequests),
+        latency: histAsObj(latencies),
+        throughput: histAsObj(throughput, totalBytes),
+        errors: errors,
+        duration: opts.duration,
+        connections: opts.connections,
+        pipelining: opts.pipelining,
+        '2xx': statusCodes[1],
+        'non2xx': statusCodes[0] + statusCodes[2] + statusCodes[3] + statusCodes[4]
+      })
+    }
   }, 1000)
 
   if (!opts.connections) {
@@ -38,6 +71,7 @@ function run (opts, cb) {
   }
 
   function record (statusCode, resBytes, responseTime) {
+    statusCodes[(parseInt(statusCode) / 100) - 1] += 1
     latencies.record(responseTime)
     bytes += resBytes
     counter++
@@ -48,24 +82,23 @@ function run (opts, cb) {
   }
 
   setTimeout(() => {
-    clearInterval(interval)
-    clients.forEach((client) => client.destroy())
-    cb(null, {
-      requests: histAsObj(requests),
-      latency: histAsObj(latencies),
-      throughput: histAsObj(throughput),
-      errors: errors
-    })
+    stop = true
   }, opts.duration * 1000)
 }
 
-function histAsObj (hist) {
-  return {
+function histAsObj (hist, total) {
+  const result = {
     average: hist.mean(),
     stddev: hist.stddev(),
     min: hist.min(),
     max: hist.max()
   }
+
+  if (total) {
+    result.total = total
+  }
+
+  return result
 }
 
 module.exports = run
--- test.js
+++ test.js
@@ -60,6 +60,10 @@ test('autocannon', (t) => {
   }, function (err, result) {
     t.error(err)
 
+    t.equal(result.duration, 2, 'duration is the same')
+    t.equal(result.connections, 2, 'connections is the same')
+    t.equal(result.pipelining, 1, 'pipelining is the default')
+
     t.ok(result.latency, 'latency exists')
     t.ok(result.latency.average, 'latency.average exists')
     t.ok(result.latency.stddev, 'latency.stddev exists')
@@ -71,14 +75,18 @@ test('autocannon', (t) => {
     t.ok(result.requests.stddev, 'requests.stddev exists')
     t.ok(result.requests.min, 'requests.min exists')
     t.ok(result.requests.max, 'requests.max exists')
+    t.ok(result.requests.total >= result.requests.average * 2 / 100 * 95, 'requests.total exists')
 
     t.ok(result.throughput, 'throughput exists')
     t.ok(result.throughput.average, 'throughput.average exists')
     t.ok(result.throughput.stddev, 'throughput.stddev exists')
     t.ok(result.throughput.min, 'throughput.min exists')
     t.ok(result.throughput.max, 'throughput.max exists')
+    t.ok(result.throughput.total >= result.throughput.average * 2 / 100 * 95, 'throughput.total exists')
 
     t.equal(result.errors, 0, 'no errors')
+    t.equal(result['2xx'], result.requests.total, '2xx codes')
+    t.equal(result.non2xx, 0, 'non 2xx codes')
 
     t.end()
   })
