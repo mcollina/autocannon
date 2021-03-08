17a93a2c6251628cc6f20aa7784afc56b4401c3a
Max Fortun <Max.Fortun@wsj.com>
2021-03-08 15:30:24 -0500

    Adding unit test for status code statistics table

--- test/printResult-process.js
+++ test/printResult-process.js
@@ -4,11 +4,11 @@ const autocannon = require('../autocannon')
 const exampleResult = require('./fixtures/example-result.json')
 const crossArgv = require('cross-argv')
 
-let opts = null;
+let opts = null
 
-if(process.argv.length > 2) {
-  const args = crossArgv(process.argv.slice(2));
-  opts = autocannon.parseArguments(args);
+if (process.argv.length > 2) {
+  const args = crossArgv(process.argv.slice(2))
+  opts = autocannon.parseArguments(args)
 }
 
 const resultStr = autocannon.printResult(exampleResult, opts)
