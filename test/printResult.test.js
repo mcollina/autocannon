455557ee6fa154272c93a414e516c823e11d3c14
elirandav <21252337+elirandav@users.noreply.github.com>
2021-07-07 19:21:10 +0300

    fix(report): align printResult to output the correct total requests (#380)
    * fix(report): align printResult to output the correct total requests
    
    Closes https://github.com/mcollina/autocannon/issues/228
    
    * fix(report): align printResult to output the correct total requests. use result.requests.sent instead of opts.amount
    
    Closes https://github.com/mcollina/autocannon/issues/228
    
    * fix(report): align printResult to output the correct total requests. removing input which is not mandatory for the test
    
    Closes https://github.com/mcollina/autocannon/issues/228

--- test/printResult.test.js
+++ test/printResult.test.js
@@ -4,6 +4,7 @@ const test = require('tap').test
 const split = require('split2')
 const path = require('path')
 const childProcess = require('child_process')
+const printResult = require('../lib/printResult')
 
 test('should stdout (print) the result', (t) => {
   const lines = [
@@ -47,4 +48,50 @@ test('should stdout (print) the result', (t) => {
       t.ok(regexp.test(line), 'line matches ' + regexp)
     })
     .on('end', t.end)
+})
+
+test('verify amount of total requests', (t) => {
+  t.plan(1)
+
+  // arrange
+  const connections = 10
+  const pipelining = 2
+  const result = {
+    connections,
+    pipelining,
+    latency: {},
+    requests: {
+      sent: connections * pipelining
+    },
+    throughput: {
+      average: 3319,
+      mean: 3319,
+      stddev: 0,
+      min: 3318,
+      max: 3318,
+      total: 3318,
+      p0_001: 3319,
+      p0_01: 3319,
+      p0_1: 3319,
+      p1: 3319,
+      p2_5: 3319,
+      p10: 3319,
+      p25: 3319,
+      p50: 3319,
+      p75: 3319,
+      p90: 3319,
+      p97_5: 3319,
+      p99: 3319,
+      p99_9: 3319,
+      p99_99: 3319,
+      p99_999: 3319
+    }
+  }
+
+  // act
+  const output = printResult(result, { })
+
+  // assert
+  const expectedRequests = connections * pipelining
+  t.match(output.includes(`${expectedRequests} requests in`), true)
 })
