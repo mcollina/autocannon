cb1fe35a1f3b400c1eb50e456e08a5bf43d8600c
Jamie King <jamie.king@aexp.com>
2024-03-18 03:44:36 -0700

    fix(printResult): tables included colors even when outputStream did not support it (#513)

--- test/printResult.test.js
+++ test/printResult.test.js
@@ -4,6 +4,8 @@ const test = require('tap').test
 const split = require('split2')
 const path = require('path')
 const childProcess = require('child_process')
+const { Writable } = require('stream')
+const ansiRegex = require('ansi-regex')
 const printResult = require('../lib/printResult')
 
 test('should stdout (print) the result', (t) => {
@@ -138,4 +140,123 @@ test('should not print when verbose(V=0) is false', (t) => {
   // act
   const output = printResult(result, { verbose: false })
   t.ok(output.split('\n').length === 2)
+})
+
+test('should print with color when color is supported', (t) => {
+  t.plan(1)
+
+  const connections = 10
+  const pipelining = 2
+  const result = {
+    connections,
+    pipelining,
+    latency: {},
+    requests: {
+      sent: connections * pipelining
+    },
+    throughput: {
+      average: 3319,
+      mean: 3319,
+      stddev: 0,
+      min: 3318,
+      max: 3318,
+      total: 3318,
+      p0_001: 3319,
+      p0_01: 3319,
+      p0_1: 3319,
+      p1: 3319,
+      p2_5: 3319,
+      p10: 3319,
+      p25: 3319,
+      p50: 3319,
+      p75: 3319,
+      p90: 3319,
+      p97_5: 3319,
+      p99: 3319,
+      p99_9: 3319,
+      p99_99: 3319,
+      p99_999: 3319
+    }
+  }
+
+  const { FORCE_COLOR, NO_COLOR, COLOR, CI, COLORTERM } = process.env
+  delete process.env.FORCE_COLOR
+  delete process.env.NO_COLOR
+  delete process.env.COLOR
+  delete process.env.CI
+  process.env.COLORTERM = 'truecolor'
+  const outputStream = new Writable({
+    write () {}
+  })
+  outputStream.isTTY = true
+
+  // act
+  const output = printResult(result, { outputStream })
+  t.ok(ansiRegex().test(output))
+
+  // cleanup
+  process.env.FORCE_COLOR = FORCE_COLOR
+  process.env.NO_COLOR = NO_COLOR
+  process.env.COLOR = COLOR
+  process.env.CI = CI
+  process.env.COLORTERM = COLORTERM
+})
+
+test('should not print with any color when color is not supported', (t) => {
+  t.plan(1)
+
+  const connections = 10
+  const pipelining = 2
+  const result = {
+    connections,
+    pipelining,
+    latency: {},
+    requests: {
+      sent: connections * pipelining
+    },
+    throughput: {
+      average: 3319,
+      mean: 3319,
+      stddev: 0,
+      min: 3318,
+      max: 3318,
+      total: 3318,
+      p0_001: 3319,
+      p0_01: 3319,
+      p0_1: 3319,
+      p1: 3319,
+      p2_5: 3319,
+      p10: 3319,
+      p25: 3319,
+      p50: 3319,
+      p75: 3319,
+      p90: 3319,
+      p97_5: 3319,
+      p99: 3319,
+      p99_9: 3319,
+      p99_99: 3319,
+      p99_999: 3319
+    }
+  }
+
+  const { FORCE_COLOR, NO_COLOR, COLOR, CI, COLORTERM } = process.env
+  delete process.env.FORCE_COLOR
+  delete process.env.NO_COLOR
+  delete process.env.COLOR
+  delete process.env.CI
+  process.env.COLORTERM = 'truecolor'
+  const outputStream = new Writable({
+    write () {}
+  })
+
+  // act
+  const output = printResult(result, { outputStream })
+  t.ok(!ansiRegex().test(output))
+
+  // cleanup
+  process.env.FORCE_COLOR = FORCE_COLOR
+  process.env.NO_COLOR = NO_COLOR
+  process.env.COLOR = COLOR
+  process.env.CI = CI
+  process.env.COLORTERM = COLORTERM
 })
